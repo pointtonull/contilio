d15225b0c5b8d3b8ec4db7993bd0757dc98f05ce
CC <carlos.cabrera@gcc.spire.com>
2025-05-01 17:32:19 +0100

    🔧 Added typing and modified factory classes, removed init migration

--- tests/factories.py
+++ tests/factories.py
@@ -4,15 +4,15 @@ from polyfactory.factories.sqlalchemy_factory import SQLAlchemyFactory
 from app import models, schemas
 
 
-class DeckModelFactory(SQLAlchemyFactory[models.Deck]):
-    __model__ = models.Deck
+class RouteModelFactory(SQLAlchemyFactory[models.Route]):
+    __model__ = models.Route
     id = None
 
 
-class CardModelFactory(SQLAlchemyFactory[models.Card]):
-    __model__ = models.Card
+class StopModelFactory(SQLAlchemyFactory[models.Stop]):
+    __model__ = models.Stop
     id = None
 
 
-class CardCreateSchemaFactory(ModelFactory[schemas.CardCreate]):
-    __model__ = schemas.CardCreate
+class StopCreateSchemaFactory(ModelFactory[schemas.StopCreate]):
+    __model__ = schemas.StopCreate
